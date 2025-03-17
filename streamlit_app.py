3898b2c77132bbe1bd9738b36f16a0d9e285ce00
unccJoseph <jgilli15@charlotte.edu>
2025-03-17 01:26:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
-st.title("Data App Assignment, on Oct 6th")
+st.title("Data App Assignment, on Oct 7th")
 
 st.write("### Input Data and Examples")
 df = pd.read_csv("Superstore_Sales_utf8.csv", parse_dates=True)
 st.dataframe(df)
+
+category_options = df["Category"].unique()
+selected_category = st.selectbox("Select a Category:", category_options)
 
 # This bar chart will not have solid bars--but lines--because the detail data is being graphed independently
 st.bar_chart(df, x="Category", y="Sales")
